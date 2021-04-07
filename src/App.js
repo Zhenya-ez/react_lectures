20f6be48102cfa711a092f890bc5425f556fd868
Yevhen_Zhuzha <evgen_1988@ukr.net>
2021-04-07 13:17:11 +0300

    www

--- src/App.js
+++ src/App.js
@@ -1,106 +1,52 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-	Redirect,
-	useLocation,
-	useParams,
-	useRouteMatch,
-	useHistory
-} from "react-router-dom";
+import React, { createContext, useContext } from "react";
 
+const CounterContext = createContext();
 
-export default function BasicExample() {
+const ContextProvider = ({children}) => {
+	const [counter, setCounter] = React.useState(0)
+
+	const incCounter = () => {
+		setCounter(counter + 1)
+	}
+
+	const decCounter = () => {
+		setCounter(counter - 1)
+	}
+
+	return (
+		<CounterContext.Provider value={{
+			counter,
+			incCounter,
+			decCounter
+		}}>
+			{children}
+		</CounterContext.Provider>
+	)
+}
+
+const Counter = () => {
+	const { counter, incCounter, decCounter } = useContext(CounterContext);
+	return (
+		<>
+		<h3>Counter : {counter}</h3>
+		<button onClick={incCounter}>inc</button>
+		<button onClick={decCounter}>dec</button>
+		</>
+	)
+}
+
+const Header = () => {
+	const { counter } = useContext(CounterContext);
+	return (
+		<h1>Header counter: {counter}</h1>
+	)
+}
+
+export default function App() {
   return (
-    <Router>
-      <div>
-				<nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-          <Link to="/posts">Posts</Link>
-          </li>
-				</ul>
-			</nav>
-
-        <Switch>
-					<Route path="/" component={Home} exact />
-					
-					{/* Те саме, просто другий вид запису */}
-					<Route path="/posts" exact>
-						<Posts />
-					</Route> 
-
-					{/* :id - динамічний параметр */}
-					<Route path="/posts/:id">
-						<PostsDetails />
-					</Route> 
-
-					<Route>
-						<h1>PAGE NOT FOUND</h1>
-					</Route>
-
-        </Switch>
-      </div>
-    </Router>
-  );
-}
-
-function Home(props) {
-	console.log(props);
-  return <h2>Home</h2>;
-}
-
-function Posts(props) {
-	console.log(props);
-	const [posts, setPosts] = React.useState([]);
-
-	const fetchData = async () => {
-		const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
-		const json = await resp.json();
-
-		setPosts(json)
-	}
-
-	React.useEffect(() => {
-		fetchData()
-	}, [])
-	return (<div>
-		<ul>
-			{posts.map(el => <Link to={`/posts/${el.id}`}><li>{el.title} - {el.id}</li></Link>)}
-		</ul>
-		</div>);
-}
-
-function PostsDetails(props) {
-	console.log(props);
-	const [post, setPost] = React.useState();
-
-	const match = useRouteMatch();
-	const {id} = useParams();
-	const location = useLocation();
-	const history = useHistory();
-
-
-	const fetchData = async () => {
-		const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-		const json = await resp.json();
-
-		setPost(json)
-	}
-
-	React.useEffect(() => {
-		fetchData()
-	}, [id])
-
-	return (<div>
-		<h1>post details</h1>
-		{post && (<> <h3>{post.title}</h3> <p>{post.body}</p> </>)}
-
-		<button onClick={() => history.push(`/posts/${+id + 1}`)}>go to next post</button>
-		</div>);
+		<ContextProvider>
+		<Header />
+		<Counter />
+		</ContextProvider>
+	);
 }
